f3025b362db45e6bf89c2846f39759bd6f8d7e89
Maxdeoro <maxdeoro@gmail.com>
2024-06-20 22:04:44 +0300

    completed block History

---
+++ wp-content/themes/childhood/functions.php
@@ -0,0 +1,15 @@
+<?php
+    add_action('wp_enqueue_scripts', 'childhood_styles');
+    add_action('wp_enqueue_scripts', 'childhood_scripts');
+
+    function childhood_styles() {
+        wp_enqueue_style('childhood-style', get_stylesheet_uri());
+    };
+
+    function childhood_scripts() {
+        wp_enqueue_script('childhood-script', get_template_directory_uri() . '/js/script.js', 
+            array(), null, true);
+    };
+
+    add_theme_support( 'custom-logo' );
+?>
